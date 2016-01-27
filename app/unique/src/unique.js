cd38f391e26c06475745ec5865229b580b896754
diralf <diralfeastarea@gmail.com>
2016-01-27 23:11:40 +0300

    created separate collection service created empty unique service created constructor for Entity

---
+++ app/unique/src/unique.js
@@ -0,0 +1,6 @@
+app.service("unique", [function () {
+    var validID = "000000000000";
+    var abc = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+    //TODO реализовать генерацию уникальных ID
+}]);
