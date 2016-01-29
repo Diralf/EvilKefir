893fae2d78f9c72d250d33bbc593001e3efe1195
diralf <diralfeastarea@gmail.com>
2016-01-29 15:52:46 +0300

    moved all method of collection in prototype

--- app/collection/src/collection.js
+++ app/collection/src/collection.js
@@ -6,121 +6,119 @@ app.service("collection", [function () {
     };
 
     this.Collection = function () {
+        this._data = {};
+    };
+
+    /**
+     * Метод для замены или добавления нового элемента в коллекцию.
+     * param {string} key          - идентификатор объекта в колекции
+     * param {object} entity       - новый объект
+     * return {object} предыдущий объект или undefined если объектов с таким ключом не было.
+     */
+    this.Collection.prototype.set = function (key, entity) {
+        var prev;
+
+        if (typeof key === 'undefined' || key == null) {
+            throw new Error("Param 'key' is not defined!");
+        }
+
+        if (typeof entity === 'undefined' || entity == null) {
+            throw new Error("Param 'entity' is not defined!");
+        }
+
+        prev = this._data[key];
+
+        this._data[key] = entity;
+
+        return prev;
+    };
+
+    /**
+     * Добавляет новый элемент. Если ключ занят - выведет ошибку
+     * return {object} только что добавленный элемент
+     */
+    this.Collection.prototype.add = function (key, entity) {
+        if (this.get(key)) {
+            throw new Error("Entity with key '" + key + "' already exists!");
+        }
+
+        this.set(key, entity);
+
+        return entity;
+    };
+
+    /**
+     * Взять из коллекции один объект по ключу
+     * return {object} объект или undefined
+     */
+    this.Collection.prototype.get = function (key) {
+        return this._data[key];
+    };
+
+    /**
+     * Проход по всем элементам колеекции и выполнение для них функции callback
+     * param {function} callback - какое либо действие над каждым элеменетом коллекции
+     *      Параметры колбека:
+     *      param {string} key      - ключ текущего объекта
+     *      param {object} entity   - текущий объект
+     *      param {number} index    - индекс итерации
+     *      param {object} entities - собственно сама коллекция
+     */
+    this.Collection.prototype.each = function (callback) {
+        var index = 0;
+        for (var prop in this._data) {
+            callback(prop, this._data[prop], index, this._data);
+            index++;
+        }
+    };
+
+    /**
+     * Удаление элемента коллекции по ключу
+     * param {string} key - ключ удаляемого элемента
+     * return {boolean} true - элемент удален. Если удаляемого элемента уже не было в коллекции то false
+     */
+    this.Collection.prototype.remove = function (key) {
+        if (typeof key === 'undefined' && key == null) {
+            throw new Error("Param 'key' is not defined!");
+        }
+
+        if (this._data[key]) {
+            delete this._data[key];
+            return true;
+        }
+
+        return false;
+    };
+
+    /**
+     * Взять размер коллекции
+     * return {number} размер коллекции
+     */
+    this.Collection.prototype.length = function () {
+        var count = 0;
+        for (var prop in this._data) {
+            count++;
+        }
+        return count;
+    };
+
+    /**
+     * Взять коллекцию
+     * return {object} сама коллекция
+     */
+    this.Collection.prototype.getCollection = function () {
+        return this._data;
+    };
+
+    /**
+     * Очистка коллекции
+     */
+    this.Collection.prototype.clear = function () {
         var self = this;
-        var entities = {};
 
-        this._data = entities;
-
-        /**
-         * Метод для замены или добавления нового элемента в коллекцию.
-         * param {string} key          - идентификатор объекта в колекции
-         * param {object} entity       - новый объект
-         * return {object} предыдущий объект или undefined если объектов с таким ключом не было.
-         */
-        this.set = function (key, entity) {
-            var prev;
-
-            if (typeof key === 'undefined' || key == null) {
-                throw new Error("Param 'key' is not defined!");
-            }
-
-            if (typeof entity === 'undefined' || entity == null) {
-                throw new Error("Param 'entity' is not defined!");
-            }
-
-            prev = entities[key];
-
-            entities[key] = entity;
-
-            return prev;
-        };
-
-        /**
-         * Добавляет новый элемент. Если ключ занят - выведет ошибку
-         * return {object} только что добавленный элемент
-         */
-        this.add = function (key, entity) {
-            if (self.get(key)) {
-                throw new Error("Entity with key '" + key + "' already exists!");
-            }
-
-            self.set(key, entity);
-
-            return entity;
-        };
-
-        /**
-         * Взять из коллекции один объект по ключу
-         * return {object} объект или undefined
-         */
-        this.get = function (key) {
-            return entities[key];
-        };
-
-        /**
-         * Проход по всем элементам колеекции и выполнение для них функции callback
-         * param {function} callback - какое либо действие над каждым элеменетом коллекции
-         *      Параметры колбека:
-         *      param {string} key      - ключ текущего объекта
-         *      param {object} entity   - текущий объект
-         *      param {number} index    - индекс итерации
-         *      param {object} entities - собственно сама коллекция
-         */
-        this.each = function (callback) {
-            var index = 0;
-            for (var prop in entities) {
-                callback(prop, entities[prop], index, entities);
-                index++;
-            }
-        };
-
-        /**
-         * Удаление элемента коллекции по ключу
-         * param {string} key - ключ удаляемого элемента
-         * return {boolean} true - элемент удален. Если удаляемого элемента уже не было в коллекции то false
-         */
-        this.remove = function (key) {
-            if (typeof key === 'undefined' && key == null) {
-                throw new Error("Param 'key' is not defined!");
-            }
-
-            if (entities[key]) {
-                delete entities[key];
-                return true;
-            }
-
-            return false;
-        };
-
-        /**
-         * Взять размер коллекции
-         * return {number} размер коллекции
-         */
-        this.length = function () {
-            var count = 0;
-            for (var prop in entities) {
-                count++;
-            }
-            return count;
-        };
-
-        /**
-         * Взять коллекцию
-         * return {object} сама коллекция
-         */
-        this.getCollection = function () {
-            return entities;
-        };
-
-        /**
-         * Очистка коллекции
-         */
-        this.clear = function () {
-            self.each(function (key) {
-                self.remove(key);
-            });
-        };
-
+        self.each(function (key) {
+            self.remove(key);
+        });
     };
 
 }]);
