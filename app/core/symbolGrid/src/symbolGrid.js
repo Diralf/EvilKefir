ee6a77f19728c63de169f905738fbd23957238e4
diralf <diralfeastarea@gmail.com>
2016-02-01 17:17:56 +0300

    refactored directories

--- app/core/symbolGrid/src/symbolGrid.js
+++ app/core/symbolGrid/src/symbolGrid.js
@@ -1,5 +1,52 @@
 app.service('symbolGrid', ['collection', function (collection) {
 
+    this.create = function () {
+        return new this.SymbolGrid();
+    };
+
+    this.SymbolGrid = function () {
+        this.data = [];
+        this.width = 0;
+        this.height = 0;
+    };
+
+    this.SymbolGrid.prototype.getRect = function (x, y, w, h) {
+        var result = [];
+
+        var xInfo = new DimensionInfo(x, w, this.width);
+
+        var yInfo = new DimensionInfo(y, h, this.height);
+
+        fixedParamsRect(xInfo);
+
+        fixedParamsRect(yInfo);
+
+        for (var i = 0; i < yInfo.length; i++) {
+            result[i] = this.data[yInfo.coord + i].substr(xInfo.coord, xInfo.length);
+        }
+
+        return result;
+    };
+
+    this.SymbolGrid.prototype.initFromText = function (text) {
+        return text.split('\n');
+    };
+
+    this.SymbolGrid.prototype.init = function (startData) {
+        if (typeof startData === 'string') {
+            this.data = this.initFromText(startData);
+        } else if (typeof startData.length !== 'undefined') {
+            this.data = startData;
+        } else {
+            throw new Error("Start data is not correct type (" + typeof startData + ")!");
+        }
+
+        var size = calcSize(this.data);
+        this.width = size.width;
+        this.height = size.height;
+    };
+
+
     function DimensionInfo(coord, length, size) {
         this.coord = coord;
         this.length = length;
@@ -40,51 +87,4 @@ app.service('symbolGrid', ['collection', function (collection) {
             height: data.length
         }
     }
-
-    this.create = function () {
-        return new this.SymbolGrid();
-    };
-
-    this.SymbolGrid = function () {
-        this.data = [];
-        this.width = 0;
-        this.height = 0;
-
-    };
-
-    this.SymbolGrid.prototype.getRect = function (x, y, w, h) {
-        var result = [];
-
-        var xInfo = new DimensionInfo(x, w, this.width);
-
-        var yInfo = new DimensionInfo(y, h, this.height);
-
-        fixedParamsRect(xInfo);
-
-        fixedParamsRect(yInfo);
-
-        for (var i = 0; i < yInfo.length; i++) {
-            result[i] = this.data[yInfo.coord + i].substr(xInfo.coord, xInfo.length);
-        }
-
-        return result;
-    };
-
-    this.SymbolGrid.prototype.initFromText = function (text) {
-        return text.split('\n');
-    };
-
-    this.SymbolGrid.prototype.init = function (startData) {
-        if (typeof startData === 'string') {
-            this.data = this.initFromText(startData);
-        } else if (typeof startData.length !== 'undefined') {
-            this.data = startData;
-        } else {
-            throw new Error("Start data is not correct type (" + typeof startData + ")!");
-        }
-
-        var size = calcSize(this.data);
-        this.width = size.width;
-        this.height = size.height;
-    };
 }]);
