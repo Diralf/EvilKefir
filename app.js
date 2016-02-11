b875bbf720d80e00322c0ccf6ff8dd248c41f788
diralf <diralfeastarea@gmail.com>
2016-02-12 01:24:12 +0300

    added checker enter and exit in game changed server on express 4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 app = angular.module("app", []);
 
-app.run(function ($rootScope, keyboardService, mapService, viewportService) {
+app.run(function ($rootScope, $window, $http, keyboardService, mapService, viewportService) {
     //console.log($rootScope);
 
     keyboardService.init();
@@ -8,6 +8,23 @@ app.run(function ($rootScope, keyboardService, mapService, viewportService) {
     viewportService.init();
 
     //mapLoader.load();
+    var onEnter = function () {
+        console.log('start game send request');
+        $http.get('/game/0').then(function () {
+            console.log('start game response recived');
+        });
+    };
+
+    var onExit = function () {
+        console.log('end game send request');
+        $http.get('/game/1').then(function () {
+            console.log('end game response recived');
+        });
+        //return ('bye bye');
+    };
+
+    onEnter();
+    $window.onbeforeunload = onExit;
 });
 
 app.config(function ($logProvider) {
