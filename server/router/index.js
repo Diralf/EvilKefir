85f9a6102de4a049a453c2e07cbf6db052ec356e
diralf <diralfeastarea@gmail.com>
2016-02-12 22:48:28 +0300

    fixed mongo connect 2

--- server/router/index.js
+++ server/router/index.js
@@ -2,30 +2,39 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('../db/mongoose');
 
+var player = mongoose.Schema({
+    ip: String,
+    date: Date,
+    checkpoint: String
+});
+var PlayerEnter = mongoose.model('PlayerEnter', player);
+var PlayerExit = mongoose.model('PlayerExit', player);
+
+
 router.route('/game/0').post(function (req, res) {
-    //var mongoose = require('mongoose');
-    //mongoose.connect('mongodb://localhost/test');
-
-    var Cat = mongoose.model('Cat', { name: String });
-
-    var kitty = new Cat({ name: 'Zildjian' });
-    kitty.save(function (err) {
-        if (err) // ...
-            console.log('meow');
+    var player = new PlayerEnter({
+        ip: req.header('x-forwarded-for') || req.connection.remoteAddress,
+        date: new Date(),
+        checkpoint: req.body.checkpoint.slice(0, 4)
     });
 
-    console.log(req.body.checkpoint);
-    console.log(new Date());
-    console.log(req.header('x-forwarded-for') || req.connection.remoteAddress);
-    console.log(req.headers['user-agent']);
-    res.end(req.header('x-forwarded-for') || req.connection.remoteAddress);
+    player.save(function () {
+    });
+
+    res.end();
 });
 
 router.route('/game/1').post(function (req, res) {
-    console.log(req.body.checkpoint);
-    console.log(new Date());
-    console.log(req.header('x-forwarded-for') || req.connection.remoteAddress);
-    res.end(req.header('x-forwarded-for') || req.connection.remoteAddress);
+    var player = new PlayerExit({
+        ip: req.header('x-forwarded-for') || req.connection.remoteAddress,
+        date: new Date(),
+        checkpoint: req.body.checkpoint.slice(0, 4)
+    });
+
+    player.save(function () {
+    });
+
+    res.end();
 });
 
 module.exports = router;
