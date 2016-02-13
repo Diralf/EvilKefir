2a72f8b7fcfcaf063584d0776f95ce9eca8b72dc
diralf <diralfeastarea@gmail.com>
2016-02-13 15:25:53 +0300

    added rect service implemented masks implemented check of collisions

---
+++ app/core/message/messageTypes.js
@@ -0,0 +1,6 @@
+app.constant('MESSAGE_TYPES', {
+    COLLISION: 'collision',
+    MOVE: 'move',
+    STEP: 'step',
+    LOOK: 'look'
+});
--- app/core/sprite/src/sprite.js
+++ app/core/sprite/src/sprite.js
@@ -1,10 +1,11 @@
-app.service('sprite', ['spriteImage', function (spriteImage) {
-    this.create = function (image) {
-        return new this.Sprite(image);
+app.service('sprite', ['spriteImage', 'rect', function (spriteImage, rect) {
+    this.create = function (image, mask) {
+        return new this.Sprite(image, mask);
     };
 
-    this.Sprite = function (image) {
+    this.Sprite = function (image, mask) {
         this.spriteImage = image || spriteImage.create();
+        this.mask = mask || new rect.Rect();
     };
 
     this.Sprite.prototype.image = function () {
