8e1fb28376f721468f92af76d279e046c3aebfcf
diralf <diralfeastarea@gmail.com>
2016-02-15 18:55:58 +0300

    made ALL map

--- app/map/src/mapService.js
+++ app/map/src/mapService.js
@@ -1,4 +1,4 @@
-app.service("mapService", ['$http', 'level', 'entityVisible', 'spriteImage', function ($http, level, entityVisible, spriteImage) {
+app.service("mapService", ['$http', '$q', 'level', 'entityVisible', 'spriteImage', function ($http, $q, level, entityVisible, spriteImage) {
     var self = this;
 
     this.levels = [];
@@ -10,30 +10,41 @@ app.service("mapService", ['$http', 'level', 'entityVisible', 'spriteImage', fun
         this.currentLevel = this.levels[0];
 
         var startArray = [];
-        for (var i = 0; i < 500; i++) {
-            startArray[i] = new Array(500 + 1).join(" "); // that will return {string}
+        for (var i = 0; i < 42 * 2; i++) {
+            startArray[i] = new Array(89 * 5).join(" "); // that will return {string}
         }
 
         this.currentLevel.tile.init(startArray);
 
-        $http.post('/sprite', {fileName: 'map/room1.txt'}).then(function (response) {
-            var matrix = [];
+        var blankroom = '';
+        for (var i = 0; i < 40; i++) {
+            blankroom += new Array(89).join(" ") + '\n'; // that will return {string}
+        }
 
-            for (var i = 0; i < 4; i++) {
-                matrix[i] = [];
-                for (var j = 0; j < 5; j++) {
-                    matrix[i][j] = response.data;
-                }
-            }
+        var maps = [
+            'map/tile/8FansRoomNice.txt',
+            'map/tile/1FirstRoom.txt',
+            'map/tile/6ForestOneRoomNice.txt',
+            'map/tile/7ForestSecondRoomNice.txt',
+            'map/tile/9LikeRoomNice.txt',
+            'map/tile/5RESTORANRoomNice.txt',
+            'map/tile/3RestRoomNice.txt',
+            'map/tile/2SecondRoom.txt',
+            'map/tile/4ThridRoomNice.txt',
+            'map/roomSmall.txt'
+        ];
+
+        $q.all(maps.map(function (item) {
+            return $http.post('/sprite', {fileName: item});
+        })).then(function (maps) {
+            var matrix = [
+                [maps[1].data, maps[7].data, maps[6].data, maps[4].data, maps[9].data],
+                [maps[5].data, maps[8].data, maps[2].data, maps[3].data, maps[0].data]
+            ];
 
             self.currentLevel.tile.initFromMatrix(matrix);
             self.currentLevel.mask.initFromMatrix(matrix);
         });
-
-        //var imageSquare = spriteImage.create('╔═╗║ ║║ ║╚═╝', 3, 4, 1, 3);
-        //this.currentLevel.layers[0].add(entityVisible.create(10, 10, imageSquare));
-
-        //this.currentLevel.layers[0].add(character.create(20, 20), 20, 20);
     };
 
     this.getRect = function (x, y, w, h) {
