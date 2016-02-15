9d430663915c6bc2b7f9f4ccb953b634acc8a57c
diralf <diralfeastarea@gmail.com>
2016-02-15 22:27:08 +0300

    fixed UI

--- app/map/src/mapService.js
+++ app/map/src/mapService.js
@@ -11,7 +11,7 @@ app.service("mapService", ['$http', '$q', 'level', 'entityVisible', 'spriteImage
 
         var startArray = [];
         for (var i = 0; i < 42 * 3; i++) {
-            startArray[i] = new Array(89 * 5).join(" "); // that will return {string}
+            startArray[i] = new Array(89 * 6).join(" "); // that will return {string}
         }
 
         this.currentLevel.tile.init(startArray);
@@ -38,9 +38,9 @@ app.service("mapService", ['$http', '$q', 'level', 'entityVisible', 'spriteImage
             return $http.post('/sprite', {fileName: item});
         })).then(function (maps) {
             var matrix = [
-                [maps[1].data, maps[7].data, maps[6].data, maps[4].data, maps[9].data],
-                [maps[5].data, maps[8].data, maps[2].data, maps[3].data, maps[0].data],
-                [maps[9].data, maps[9].data, maps[9].data, maps[9].data, maps[9].data]
+                [maps[1].data, maps[7].data, maps[6].data, maps[4].data, maps[9].data, maps[9].data],
+                [maps[5].data, maps[8].data, maps[2].data, maps[3].data, maps[0].data, maps[9].data],
+                [maps[9].data, maps[9].data, maps[9].data, maps[9].data, maps[9].data, maps[9].data]
             ];
 
             self.currentLevel.tile.initFromMatrix(matrix);
