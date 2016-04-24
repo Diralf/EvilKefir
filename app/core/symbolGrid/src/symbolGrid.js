25591e170552e0decc0d7c69035e431fad247431
diralf <diralfeastarea@gmail.com>
2016-04-24 16:24:17 +0300

    refactored symbolGrid folder

--- app/core/symbolGrid/src/symbolGrid.js
+++ app/core/symbolGrid/src/symbolGrid.js
@@ -1,143 +1,166 @@
-app.service('symbolGrid', ['collection', function (collection) {
+(function () {
+    'use strict';
 
-    this.create = function () {
-        return new this.SymbolGrid();
-    };
+    angular
+        .module('app')
+        .service('symbolGrid', symbolGrid);
 
-    this.SymbolGrid = function () {
-        this.data = [];
-        this.width = 0;
-        this.height = 0;
-    };
+    // TODO запилить все классы с помошью value, с композиционной передачей зависимостей
 
-    this.SymbolGrid.prototype.getRect = function (x, y, w, h) {
-        var result = [];
+    symbolGrid.$inject = [];
 
-        var xInfo = new DimensionInfo(x, w, this.width);
+    function symbolGrid() {
 
-        var yInfo = new DimensionInfo(y, h, this.height);
+        this.create = create;
 
-        fixedParamsRect(xInfo);
+        this.SymbolGrid = SymbolGrid;
+        this.SymbolGrid.prototype.getRect = getRect;
+        this.SymbolGrid.prototype.initFromText = initFromText;
+        this.SymbolGrid.prototype.initFromStringArray = initFromStringArray;
+        this.SymbolGrid.prototype.initFromMatrix = initFromMatrix;
+        this.SymbolGrid.prototype.init = init;
 
-        fixedParamsRect(yInfo);
+        ////////////////////////////////////////////////
 
-        for (var i = 0; i < yInfo.length; i++) {
-            result[i] = this.data[yInfo.coord + i].slice(xInfo.coord, xInfo.coord + xInfo.length);
+        function create() {
+            return new this.SymbolGrid();
         }
 
-        return result;
-    };
+        function SymbolGrid() {
+            this.data = [];
+            this.width = 0;
+            this.height = 0;
+        }
 
-    this.SymbolGrid.prototype.initFromText = function (text) {
-        return this.initFromStringArray(text.split('\n'));
-    };
+        function getRect(x, y, w, h) {
+            var result = [];
 
-    this.SymbolGrid.prototype.initFromStringArray = function (strings) {
-        var grid = [];
+            var xInfo = new DimensionInfo(x, w, this.width);
 
-        strings.forEach(function (line, index) {
-            grid[index] = line.split('');
-        });
+            var yInfo = new DimensionInfo(y, h, this.height);
 
-        return grid;
-    };
+            fixedParamsRect(xInfo);
 
-    this.SymbolGrid.prototype.initFromMatrix = function (textMatrix) {
-        var mapArray = [];
-        var widthChunk = 0;
+            fixedParamsRect(yInfo);
 
-        textMatrix.forEach(function (line) {
-            var chunkArray = []; //[arrayLines, arrayLines, arrayLines]
-
-            line.forEach(function (chunk) {
-                chunkArray.push(chunk.split('\n').map(function (line) {
-                    return line.replace(new RegExp(String.fromCharCode(13), 'g'), '');
-                })); // push(arrayLines);
-            });
-
-            widthChunk = chunkArray[0][0].length;
-
-            var linesLine;
-            chunkArray.forEach(function (chunkLines) {
-                if (!linesLine) {
-                    linesLine = chunkLines.map(function (line) {
-                        return getCorrectLine(line, widthChunk);
-                    });
-                } else {
-                    chunkLines.forEach(function (line, index) {
-                         linesLine[index] += getCorrectLine(line, widthChunk);
-                    });
-                }
-            });
-
-            mapArray = mapArray.concat(linesLine);
-        });
-
-        this.init(mapArray);
-
-        function getCorrectLine(line, width) {
-            var widthLine = line.length;
-            var addition = '';
-            if (widthLine < width) {
-                addition = new Array(width - widthLine + 1).join(' ');
+            for (var i = 0; i < yInfo.length; i++) {
+                result[i] = this.data[yInfo.coord + i].slice(xInfo.coord, xInfo.coord + xInfo.length);
             }
-            return line + addition;
-        }
-    };
 
-    this.SymbolGrid.prototype.init = function (startData) {
-        if (typeof startData === 'string') {
-            this.data = this.initFromText(startData);
-        } else if (typeof startData.length !== 'undefined') {
-            this.data = this.initFromStringArray(startData);
-        } else {
-            throw new Error("Start data is not correct type (" + typeof startData + ")!");
+            return result;
         }
 
-        var size = calcSize(this.data);
-        this.width = size.width;
-        this.height = size.height;
-    };
-
-
-    function DimensionInfo(coord, length, size) {
-        this.coord = coord;
-        this.length = length;
-        this.size = size;
-    };
-
-    function fixedParamsRect (info) {
-        if (info.length < 0) {
-            info.coord += info.length + 1;
-            info.length = -info.length;
+        function initFromText(text) {
+            return this.initFromStringArray(text.split('\n'));
         }
 
-        if (info.coord < 0) {
-            info.length += info.coord;
-            info.coord = 0;
-        }
+        function initFromStringArray(strings) {
+            var grid = [];
 
-        if (info.coord + info.length > info.size) {
-            info.length = info.size - info.coord;
-        }
-    };
-
-    function calcSize(data) {
-        var width = 0;
-
-        if (data.length > 0) {
-            width = data[0].length;
-
-            data.forEach(function (item) {
-                if (item.length < width) {
-                    width = item.length;
-                }
+            strings.forEach(function (line, index) {
+                grid[index] = line.split('');
             });
+
+            return grid;
         }
 
-        return {
-            width: width,
-            height: data.length
+        function initFromMatrix(textMatrix) {
+            var mapArray = [];
+            var widthChunk = 0;
+
+            textMatrix.forEach(function (line) {
+                var chunkArray = []; //[arrayLines, arrayLines, arrayLines]
+
+                line.forEach(function (chunk) {
+                    chunkArray.push(chunk.split('\n').map(function (line) {
+                        return line.replace(new RegExp(String.fromCharCode(13), 'g'), '');
+                    })); // push(arrayLines);
+                });
+
+                widthChunk = chunkArray[0][0].length;
+
+                var linesLine;
+                chunkArray.forEach(function (chunkLines) {
+                    if (!linesLine) {
+                        linesLine = chunkLines.map(function (line) {
+                            return getCorrectLine(line, widthChunk);
+                        });
+                    } else {
+                        chunkLines.forEach(function (line, index) {
+                            linesLine[index] += getCorrectLine(line, widthChunk);
+                        });
+                    }
+                });
+
+                mapArray = mapArray.concat(linesLine);
+            });
+
+            this.init(mapArray);
+
+            function getCorrectLine(line, width) {
+                var widthLine = line.length;
+                var addition = '';
+                if (widthLine < width) {
+                    addition = new Array(width - widthLine + 1).join(' ');
+                }
+                return line + addition;
+            }
+        }
+
+        function init(startData) {
+            if (typeof startData === 'string') {
+                this.data = this.initFromText(startData);
+            } else if (typeof startData.length !== 'undefined') {
+                this.data = this.initFromStringArray(startData);
+            } else {
+                throw new Error('Start data is not correct type (' + typeof startData + ')!');
+            }
+
+            var size = calcSize(this.data);
+            this.width = size.width;
+            this.height = size.height;
+        }
+
+        function DimensionInfo(coord, length, size) {
+            this.coord = coord;
+            this.length = length;
+            this.size = size;
+        }
+
+        function fixedParamsRect (info) {
+            if (info.length < 0) {
+                info.coord += info.length + 1;
+                info.length = -info.length;
+            }
+
+            if (info.coord < 0) {
+                info.length += info.coord;
+                info.coord = 0;
+            }
+
+            if (info.coord + info.length > info.size) {
+                info.length = info.size - info.coord;
+            }
+        }
+
+        function calcSize(data) {
+            var width = 0;
+
+            if (data.length > 0) {
+                width = data[0].length;
+
+                data.forEach(function (item) {
+                    if (item.length < width) {
+                        width = item.length;
+                    }
+                });
+            }
+
+            return {
+                width: width,
+                height: data.length
+            };
         }
     }
-}]);
+
+})();
