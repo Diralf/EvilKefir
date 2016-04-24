d177600cf54cf96d64d5f84d370846c226bacfc6
diralf <diralfeastarea@gmail.com>
2016-04-24 07:53:42 +0300

    refactored control folder

---
+++ app/control/keyboard/src/keys.constant.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .constant('KEYS', {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down',
+            87: 'W',
+            65: 'A',
+            83: 'S',
+            68: 'D'
+        });
+
+})();
---
+++ app/control/mouse/src/mouse.service.js
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .service('mouseService', mouseService);
+    //TODO убрать Service в названии
+
+    mouseService.$inject = ['$log'];
+
+    function mouseService($log) {
+        var mouseHandlers = {};
+
+        this.emitMouseEvent = emitMouseEvent;
+        this.addMouseHandler = addMouseHandler;
+
+        function emitMouseEvent(event, evtObj, cellX, cellY) {
+            if (!mouseHandlers[event]) {
+                $log.error('mouseEvent: ' + event + ' - no handlers');
+                return;
+            }
+            mouseHandlers[event].forEach(function (handleItem, index) {
+                handleItem(evtObj, +cellX, +cellY, function (result) {
+                    $log.debug('handling ' + index + ' ' + event + ' finished - result: ' + result);
+                });
+            });
+        }
+
+        function addMouseHandler(event, handler) {
+            if (!mouseHandlers[event]) {
+                mouseHandlers[event] = [];
+            }
+            mouseHandlers[event].push(handler);
+        }
+    }
+
+})();
