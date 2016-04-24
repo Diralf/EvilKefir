f82e4e74589dcab81d0837495ab8e26bf54d34ce
diralf <diralfeastarea@gmail.com>
2016-04-24 16:26:05 +0300

    refactored unique folder

--- app/core/unique/src/unique.js
+++ app/core/unique/src/unique.js
@@ -1,8 +1,19 @@
-app.service("unique", [function () {
-    var validID = "000000000000";
-    var abc = "0123456789abcdefghijklmnopqrstuvwxyz";
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .service('unique', unique);
+
+    unique.$inject = [];
+
+    function unique() {
+        var validID = '000000000000';
+        var abc = '0123456789abcdefghijklmnopqrstuvwxyz';
 
 
-    this.value = 100;
-    //TODO реализовать генерацию уникальных ID
-}]);
+        this.value = 100;
+        //TODO реализовать генерацию уникальных ID
+    }
+
+})();
