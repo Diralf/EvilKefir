7ade9aa5c6df113209cb77581879854c3b998f44
diralf <diralfeastarea@gmail.com>
2016-04-24 14:53:13 +0300

    refactored rect folder

--- app/core/rect/src/rect.js
+++ app/core/rect/src/rect.js
@@ -1,42 +1,60 @@
-app.service('rect', ['point', function (point) {
-    this.Rect = function (x, y, w, h) {
-        this.place = new point.Point(x || 0, y || 0);
-        this.size = new point.Point(w || 0, h || 0);
+(function () {
+    'use strict';
 
-        Object.defineProperty(this, "x", {
-            get: function() {
-                return this.place.x;
-            },
-            set: function (nx) {
-                this.place.moveIn(nx, this.place.y);
-            }
-        });
+    angular
+        .module('app')
+        .service('rect', rect);
 
-        Object.defineProperty(this, "y", {
-            get: function() {
-                return this.place.y;
-            },
-            set: function (ny) {
-                this.place.moveIn(this.place.x, ny);
-            }
-        });
+    // TODO запилить все классы с помошью value, с композиционной передачей зависимостей
 
-        Object.defineProperty(this, "w", {
-            get: function() {
-                return this.size.x;
-            },
-            set: function (nw) {
-                this.size.moveIn(nw, this.size.y);
-            }
-        });
+    rect.$inject = ['point'];
 
-        Object.defineProperty(this, "h", {
-            get: function() {
-                return this.size.y;
-            },
-            set: function (nh) {
-                this.size.moveIn(this.size.x, nh);
-            }
-        });
+    function rect(point) {
+
+        this.Rect = Rect;
+
+        //////////////////////////////////////////////////
+
+        function Rect(x, y, w, h) {
+            this.place = new point.Point(x || 0, y || 0);
+            this.size = new point.Point(w || 0, h || 0);
+
+            Object.defineProperty(this, 'x', {
+                get: function() {
+                    return this.place.x;
+                },
+                set: function (nx) {
+                    this.place.moveIn(nx, this.place.y);
+                }
+            });
+
+            Object.defineProperty(this, 'y', {
+                get: function() {
+                    return this.place.y;
+                },
+                set: function (ny) {
+                    this.place.moveIn(this.place.x, ny);
+                }
+            });
+
+            Object.defineProperty(this, 'w', {
+                get: function() {
+                    return this.size.x;
+                },
+                set: function (nw) {
+                    this.size.moveIn(nw, this.size.y);
+                }
+            });
+
+            Object.defineProperty(this, 'h', {
+                get: function() {
+                    return this.size.y;
+                },
+                set: function (nh) {
+                    this.size.moveIn(this.size.x, nh);
+                }
+            });
+        }
     }
-}]);
+
+})();
