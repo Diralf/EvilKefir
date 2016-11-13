01ca4358567b7b5df18729d968d07946a0877753
diralf <diralfeastarea@gmail.com>
2016-11-13 21:08:24 +0300

    block phone

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ app = angular.module("app", []);
 app.run(['$rootScope', '$http', 'keyboardService', 'mapService', 'viewportService',
     function ($rootScope, $http, keyboardService, mapService, viewportService) {
     //console.log($rootScope);
+    $rootScope.isNotDesktop = null;
+        $rootScope.deviceMessage = '';
 
     keyboardService.init();
     mapService.init();
@@ -12,7 +14,8 @@ app.run(['$rootScope', '$http', 'keyboardService', 'mapService', 'viewportServic
     var onEnter = function () {
         //console.log('start game send request');
         $http.get('/isdesk').then(function (res) {
-            $rootScope.isNotDesktop = !response.device.isDesktop;
+            $rootScope.isNotDesktop = res.data != 'desktop';
+            $rootScope.deviceMessage = "Sorry bro it's only for PC Master race";
         });
 
         $http.post('/game/0', {checkpoint: "cehck enter"}).then(function (response) {
