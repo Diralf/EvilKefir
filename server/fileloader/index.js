d0dfb79fb6f9c006577003bfb89940fd62194d0d
Dzmitry Kazlou1 <Dzmitry_Kazlou1@epam.com>
2018-06-22 15:47:27 +0300

    angular 2 integration

--- server/fileloader/index.js
+++ server/fileloader/index.js
@@ -2,8 +2,6 @@ var fs = require("mz/fs");
 
 module.exports = async function loadFile(fileName) {
     try {
-        //let file = await fs.open(fileName, 'r');
-        //let stats = await fs.stat(fileName);
         let [file, stats] = await Promise.all([fs.open(fileName, 'r'), fs.stat(fileName)]);
         let buffer = new Buffer(stats.size);
 
@@ -17,50 +15,3 @@ module.exports = async function loadFile(fileName) {
         throw e;
     }
 };
-/*
-module.exports = (fileName, callback) => {
-    console.log(fs.exists(fileName));
-    fs.exists(fileName, exists => {
-        if (exists) {
-            fs.stat(fileName, (error, stats) => {
-                fs.open(fileName, "r", (error, fd) => {
-                    let buffer = new Buffer(stats.size);
-
-                    fs.read(fd, buffer, 0, buffer.length, null, (error, bytesRead, buffer) => {
-                        let data = buffer.toString("utf8", 0, buffer.length);
-
-                        console.log(fileName + "loaded");
-
-                        callback(data);
-                        fs.close(fd);
-                    });
-                });
-            });
-        } else {
-            console.log('not exists');
-        }
-    });
-};
-*/
-// module.exports = function (fileName, callback) {
-//     fs.exists(fileName, function(exists) {
-//         if (exists) {
-//             fs.stat(fileName, function(error, stats) {
-//                 fs.open(fileName, "r", function(error, fd) {
-//                     var buffer = new Buffer(stats.size);
-
-//                     fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-//                         var data = buffer.toString("utf8", 0, buffer.length);
-
-//                         console.log(fileName + "loaded");
-
-//                         callback(data);
-//                         fs.close(fd);
-//                     });
-//                 });
-//             });
-//         } else {
-//             console.log('not exist');
-//         }
-//     });
-// };
